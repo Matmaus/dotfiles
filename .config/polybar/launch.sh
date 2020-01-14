b10d7dd38dd3d98e3300c76bb973ddfec5caea0c
Matmaus <matusjas.work@gmail.com>
2020-01-14 17:41:17 +0100

    Modify i3, polybar and compton

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 MONITOR=eDP1 polybar -r -c ~/.config/polybar/config PolybarTop &
 MONITOR=eDP1 polybar -r -c ~/.config/polybar/config PolybarBot &
-# MONITOR=DP2-2 polybar -r -c ~/.config/polybar/config PolybarTop &
-# MONITOR=DP2-2 polybar -r -c ~/.config/polybar/config PolybarBot &
-# MONITOR=HDMI1 polybar -r -c ~/.config/polybar/config PolybarTop &
-# MONITOR=HDMI1 polybar -r -c ~/.config/polybar/config PolybarBot &
+MONITOR=DP2-2 polybar -r -c ~/.config/polybar/config PolybarTop &
+MONITOR=DP2-2 polybar -r -c ~/.config/polybar/config PolybarBot &
+MONITOR=HDMI1 polybar -r -c ~/.config/polybar/config PolybarTop &
+MONITOR=HDMI1 polybar -r -c ~/.config/polybar/config PolybarBot &
---
+++ doom/config.el
@@ -0,0 +1,17 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+ (add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+(defun toggle-transparency ()
+   (interactive)
+   (let ((alpha (frame-parameter nil 'alpha)))
+     (set-frame-parameter
+      nil 'alpha
+      (if (eql (cond ((numberp alpha) alpha)
+                     ((numberp (cdr alpha)) (cdr alpha))
+                     ;; Also handle undocumented (<active> <inactive>) form.
+                     ((numberp (cadr alpha)) (cadr alpha)))
+               100)
+          '(85 . 50) '(100 . 100)))))
+ (global-set-key (kbd "C-c t") 'toggle-transparency)
